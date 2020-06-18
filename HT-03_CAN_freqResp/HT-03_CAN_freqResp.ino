6ab71f8025aa00234da04b4d39523335d9abe83d
t-kamimura <tenjinsan@gmail.com>
2020-06-18 18:36:15 +0900

    freqRespを関数化してプログラムの可読性を上げた．

--- HT-03_CAN_freqResp/HT-03_CAN_freqResp.ino
+++ HT-03_CAN_freqResp/HT-03_CAN_freqResp.ino
@@ -20,8 +20,8 @@
 #define LOOPTIME 40
 
 int A = 0.25*1024;
-double freq = 10; // [Hz]
-double omega = 3.14*2/freq;
+double freq = 1; // [Hz]
+double omega = 2*3.14*freq;
 
 int pos = 0;
 int vel = 0;
@@ -44,6 +44,110 @@ const int SPI_CS_PIN = 10;
 MCP_CAN CAN(SPI_CS_PIN); //set CS PIN
 
 unsigned char can_msg[8];
+unsigned char len = 0;
+unsigned char buf[6];
+
+void motor_enable()
+{
+  can_msg[0] = 0xFF;
+  can_msg[1] = 0xFF;
+  can_msg[2] = 0xFF;
+  can_msg[3] = 0xFF;
+  can_msg[4] = 0xFF;
+  can_msg[5] = 0xFF;
+  can_msg[6] = 0xFF;
+  can_msg[7] = 0xFC;
+  // SERIAL.println("sending CAN msg...");
+  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  delay(1000);
+}
+
+void motor_disable()
+{
+  can_msg[0] = 0xFF;
+  can_msg[1] = 0xFF;
+  can_msg[2] = 0xFF;
+  can_msg[3] = 0xFF;
+  can_msg[4] = 0xFF;
+  can_msg[5] = 0xFF;
+  can_msg[6] = 0xFF;
+  can_msg[7] = 0xFD;
+  // SERIAL.println("sending CAN msg...");
+  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  delay(500);
+}
+
+void motor_zeroPosition()
+{
+  can_msg[0] = 0xFF;
+  can_msg[1] = 0xFF;
+  can_msg[2] = 0xFF;
+  can_msg[3] = 0xFF;
+  can_msg[4] = 0xFF;
+  can_msg[5] = 0xFF;
+  can_msg[6] = 0xFF;
+  can_msg[7] = 0xFE;
+  // SERIAL.println("sending CAN msg...");
+  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+}
+
+void motor_writeCmd(int pos, int vel, int kp, int kd, int ff)
+{
+  // 符号なし変数に変換
+  upos = (unsigned int)(pos - 32768);             // 16 bit
+  uvel = (unsigned int)(vel - 2048); // 12 bit
+  ukp = (unsigned int)(kp);          // 12 bit (defined as positive value only)
+  ukd = (unsigned int)(kd);          // 12 bit (defined as positive value only)
+  uff = (unsigned int)(ff - 2048);   // 12 bit
+
+  // 入力をプロンプトに合わせて変換
+  can_msg[0] = upos >> 8;
+  can_msg[1] = upos & 0x00FF;
+  can_msg[2] = (uvel >> 4) & 0xFF;
+  can_msg[3] = ((uvel & 0x000F) << 4) + ((ukp >> 8) & 0xFF);
+  can_msg[4] = ukp & 0xFF;
+  can_msg[5] = ukd >> 4;
+  can_msg[6] = ((ukd & 0x000F) << 4) + (uff >> 8);
+  can_msg[7] = uff & 0xff;
+
+  // CAN通信で送る
+  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  
+  SERIAL.print(", ");
+  SERIAL.print(pos);
+  SERIAL.print(", ");
+  SERIAL.print(vel);
+  SERIAL.print(", ");
+  SERIAL.print(ff);
+}
+
+void motor_readState()
+{
+  if (CAN_MSGAVAIL == CAN.checkReceive()) //check if data coming
+  {
+    CAN.readMsgBuf(&len, buf); //read data, len: data length, buf: data buf
+
+    unsigned long canId = CAN.getCanId();
+    unsigned int id = buf[0];
+    unsigned int upos_cur = (buf[1] << 8) + buf[2];
+    unsigned int uvel_cur = (buf[3] << 4) + ((buf[4] & 0xF0) >> 4);
+    unsigned int ucur_cur = ((buf[4] & 0x0F) << 8) + buf[5];
+
+    int pos_cur = upos_cur + 32768;
+    int vel_cur = uvel_cur - 2048;
+    int cur_cur = ucur_cur - 2048;
+
+    // SERIAL.print("CUR: ID: ");
+    // SERIAL.print(id);
+    SERIAL.print(", ");
+    SERIAL.print(pos_cur);
+    SERIAL.print(", ");
+    SERIAL.print(vel_cur);
+    SERIAL.print(",  ");
+    SERIAL.print(cur_cur);
+    SERIAL.println();
+  }
+}
 
 void setup()
 {
@@ -63,33 +167,12 @@ void setup()
   digitalWrite(LED2, HIGH);
   delay(1000);
 
-  // 現在位置をゼロにする
   // SERIAL.print("position initializing...");
-  can_msg[0] = 0xFF;
-  can_msg[1] = 0xFF;
-  can_msg[2] = 0xFF;
-  can_msg[3] = 0xFF;
-  can_msg[4] = 0xFF;
-  can_msg[5] = 0xFF;
-  can_msg[6] = 0xFF;
-  can_msg[7] = 0xFE;
-  // SERIAL.println("sending CAN msg...");
-  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  motor_zeroPosition();  // 現在位置をゼロということにする
   digitalWrite(LED3, HIGH);
   delay(1000);
 
-  // モーターモードに入る
-  // SERIAL.print("Motor ctrl ON...");
-  can_msg[0] = 0xFF;
-  can_msg[1] = 0xFF;
-  can_msg[2] = 0xFF;
-  can_msg[3] = 0xFF;
-  can_msg[4] = 0xFF;
-  can_msg[5] = 0xFF;
-  can_msg[6] = 0xFF;
-  can_msg[7] = 0xFC;
-  // SERIAL.println("sending CAN msg...");
-  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  motor_enable(); // モーターモードに入る
   delay(1000);
 
   SERIAL.println("TIMER, TGTPOS, TGTVEL, TGTFF, CURPOS, CURVEL, CURCUR");
@@ -99,87 +182,31 @@ void setup()
 
 void loop()
 {
-  unsigned char len = 0;
-  unsigned char buf[6];
 
   for (int i = 0; i < 150; i++)
   {
     timer[1] = millis();
+    SERIAL.print(timer[1] - timer[0]);
 
-    pos = A * sin(omega * (timer[1] - timer[0]));
-    upos = pos - 32768;
-
-    // 入力をプロンプトに合わせて変換
-    can_msg[0] = upos >> 8;
-    can_msg[1] = upos & 0x00FF;
-    can_msg[2] = (uvel >> 4) & 0xFF;
-    can_msg[3] = ((uvel & 0x000F) << 4) + ((ukp >> 8) & 0xFF);
-    can_msg[4] = ukp & 0xFF;
-    can_msg[5] = ukd >> 4;
-    can_msg[6] = ((ukd & 0x000F) << 4) + (uff >> 8);
-    can_msg[7] = uff & 0xff;
-
-    SERIAL.print(timer[1]-timer[0]);
-    SERIAL.print(", ");
-    SERIAL.print(pos);
-    SERIAL.print(", ");
-    SERIAL.print(vel);
-    SERIAL.print(", ");
-    SERIAL.print(ff);
-    // SERIAL.println();
-    CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+    pos = A * sin(omega * (timer[1] - timer[0]) * 0.001);
+    motor_writeCmd(pos, vel, kp, kd, ff);
 
     // SERIAL.println("receiving CAN msg...");
-
-    if (CAN_MSGAVAIL == CAN.checkReceive()) //check if data coming
+    motor_readState();
+    
+    timer[2] = millis() - timer[1];
+    if (timer[2] < LOOPTIME)
     {
-      CAN.readMsgBuf(&len, buf); //read data, len: data length, buf: data buf
-
-      unsigned long canId = CAN.getCanId();
-      unsigned int id = buf[0];
-      unsigned int upos_cur = (buf[1] << 8) + buf[2];
-      unsigned int uvel_cur = (buf[3] << 4) + ((buf[4] & 0xF0) >> 4);
-      unsigned int ucur_cur = ((buf[4] & 0x0F) << 8) + buf[5];
-
-      int pos_cur = upos_cur + 32768;
-      int vel_cur = uvel_cur - 2048;
-      int cur_cur = ucur_cur - 2048;
-
-      // SERIAL.print("CUR: ID: ");
-      // SERIAL.print(id);
-      SERIAL.print(", ");
-      SERIAL.print(pos_cur);
-      SERIAL.print(", ");
-      SERIAL.print(vel_cur);
-      SERIAL.print(",  ");
-      SERIAL.print(cur_cur);
-      SERIAL.println();
-
-      timer[2] = millis() - timer[1];
-      if (timer[2] < LOOPTIME)
-      {
-        delay(LOOPTIME - timer[2]);
-      }
-      else
-      {
-        SERIAL.print("time shortage: ");
-        SERIAL.println(timer[2] - LOOPTIME);
-      }
+      delay(LOOPTIME - timer[2]);
+    }
+    else
+    {
+      SERIAL.print("time shortage: ");
+      SERIAL.println(timer[2] - LOOPTIME);
     }
   }
 
-  // SERIAL.print("Motor ctrl OFF...");
-  can_msg[0] = 0xFF;
-  can_msg[1] = 0xFF;
-  can_msg[2] = 0xFF;
-  can_msg[3] = 0xFF;
-  can_msg[4] = 0xFF;
-  can_msg[5] = 0xFF;
-  can_msg[6] = 0xFF;
-  can_msg[7] = 0xFD;
-
-  // SERIAL.println("sending CAN msg...");
-  CAN.sendMsgBuf(MOTOR_ADDRESS, 0, 8, can_msg);
+  motor_disable();
   delay(500);
   // SERIAL.println("Program finish!");
   digitalWrite(LED3, LOW);
