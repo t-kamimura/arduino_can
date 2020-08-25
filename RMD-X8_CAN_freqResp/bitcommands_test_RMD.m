273d5a8299e66acb7ddd2f1e3fc794ce4e83a18f
t-kamimura <tenjinsan@gmail.com>
2020-08-25 17:35:52 +0900

    ReadMultiAngleで負の値が読めない問題を解決するためのサンプルプログラムを追加

---
+++ RMD-X8_CAN_freqResp/bitcommands_test_RMD.m
@@ -0,0 +1,30 @@
+clear
+close all
+
+pos = int64(-128);
+dec2bin(pos)
+
+% CANメッセージ(7バイト)に変換
+pos_msg(1) = bitand(pos,255);
+pos_msg(2) = bitand(pos/2^8 ,255);
+pos_msg(3) = bitand(pos/2^16,255);
+pos_msg(4) = bitand(pos/2^24,255);
+pos_msg(5) = bitand(pos/2^32,255);
+pos_msg(6) = bitand(pos/2^40,255);
+pos_msg(7) = bitand(pos/2^48,255);
+
+
+for i = 1:7
+    dec2bin(pos_msg(i))
+end
+
+
+% posに変換
+for i = 1:7
+    if pos_msg(i) > 128
+        pos_msg(i) = pos_msg(i)-256;
+    end
+end
+pos_received = int64(bitshift(pos_msg(7), 48) + bitshift(pos_msg(6), 40) + bitshift(pos_msg(5), 32) + bitshift(pos_msg(4), 24) + bitshift(pos_msg(3), 16) + bitshift(pos_msg(2), 8) + pos_msg(1));
+
+dec2bin(pos_received)
